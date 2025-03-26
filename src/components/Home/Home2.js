4088317565d4fae56e8476db8d60e421de3a8da6
aryan25798 <aryan25798@gmail.com>
2025-03-26 12:34:02 +0530

    k

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in"
+                  href="https://www.linkedin.com/in/aryan-a506b8288/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
@@ -97,7 +97,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/__a_rya_n_/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
