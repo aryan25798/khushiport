8324b820890f565084113b85262a1beb1bb499f2
aryan25798 <aryan25798@gmail.com>
2025-03-26 20:07:42 +0530

    final

--- src/components/About/Github.js
+++ src/components/About/Github.js
@@ -9,29 +9,7 @@ function Github() {
         Days I <strong className="purple">Code</strong>
       </h1>
       <GitHubCalendar
-        username="aryan25798"  // Updated with your GitHub username
-        blockSize={15}
-        blockMargin={5}
-        color="#c084f5"
-        fontSize={16}
-      />
-    </Row>
-  );
-}
-
-export default Github;
-import React from "react";
-import GitHubCalendar from "react-github-calendar";
-import { Row } from "react-bootstrap";
-
-function Github() {
-  return (
-    <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-      <h1 className="project-heading" style={{ paddingBottom: "20px" }}>
-        Days I <strong className="purple">Code</strong>
-      </h1>
-      <GitHubCalendar
-        username="aryan25798"  // Updated with your GitHub username
+        username="aryan25798" // Update this to the correct GitHub username
         blockSize={15}
         blockMargin={5}
         color="#c084f5"
